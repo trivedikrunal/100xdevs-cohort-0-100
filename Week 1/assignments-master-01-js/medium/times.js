257fbf99e3a79a5aa581618aa350d8d32a6a2510
Krunal Trivedi <137049008+trivedikrunal@users.noreply.github.com>
2024-09-05 11:13:58 +0530

    Answer times.js
    Answer

--- Week 1/assignments-master-01-js/medium/times.js
+++ Week 1/assignments-master-01-js/medium/times.js
@@ -9,5 +9,15 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+    let startTime = new Date().getTime();
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }   
+    let endTime = new Date().getTime();
+    let elapsedTime = (endTime - startTime) / 1000;
+    return elapsedTime;
 }
+console.log('Time for sum from 1 to 100:', calculateTime(100), 'seconds');
+console.log('Time for sum from 1 to 100000:', calculateTime(100000), 'seconds');
+console.log('Time for sum from 1 to 1000000000:', calculateTime(1000000000), 'seconds');
